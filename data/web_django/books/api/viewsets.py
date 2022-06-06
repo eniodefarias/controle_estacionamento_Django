932022c44a0ad5ea56e2fad94bb89a450c7c703b
enio ricardo de farias <eniodefarias@gmail.com>
2022-06-06 00:11:39 -0300

    django ainda não rolou

---
+++ data/web_django/books/api/viewsets.py
@@ -0,0 +1,7 @@
+from resr_framework import viewsets
+from books.api import serializers
+
+
+class BooksViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.BooksSerializer
+    queryset = models.Books.objects.all()
